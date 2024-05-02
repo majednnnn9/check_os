97660064f2913d3dfc299d58a35fbf66001e4379
majednnnn9 <tt292665@gmail.com>
2024-05-02 23:31:55 +0100

    update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const os = require('os');
+app.get('/', (req, res) => {
+    res.json({ os: os.platform() })
+})
+
+
+app.listen(5000, () => {
+    console.log("runing is server is port ")
+})
